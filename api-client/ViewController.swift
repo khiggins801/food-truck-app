496d062e04b255e5358e6e11f982055bbf809086
Kathy Higgins <khiggins@ehsms.com>
2017-02-14 09:58:56 -0700

    Initial Commit

---
+++ api-client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  api-client
+//
+//  Created by Kathy Higgins on 2/14/17.
+//  Copyright © 2017 ColoApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ api-clientTests/api_clientTests.swift
@@ -0,0 +1,36 @@
+//
+//  api_clientTests.swift
+//  api-clientTests
+//
+//  Created by Kathy Higgins on 2/14/17.
+//  Copyright © 2017 ColoApps. All rights reserved.
+//
+
+import XCTest
+@testable import api_client
+
+class api_clientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
